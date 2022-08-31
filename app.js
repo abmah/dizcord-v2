b97dd3b9ca370d5069ad073927756d66e43ed034
abmah <ab.mahasnh@gmail.com>
2022-08-31 13:39:55 +0300

    third commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.join(__dirname + "/public")));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("/public"));
   app.get("/*", function (req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "./public"));
   });
 }
 const cors = require("cors");
